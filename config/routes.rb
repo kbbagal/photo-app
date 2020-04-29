5f4d66b845c81bf3f9f9cbebef3184e271db4cde
= <=>
2020-04-29 06:48:38 +0530

    Add devise and auth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'welcome#index'
-  get 'welcome', to: 'welcome#index'
-  get 'welcome_test', to: 'welcome#test'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
